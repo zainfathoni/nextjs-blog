e8192b899987a4a3ca3120a5bcc74503e914eca1
Zain Fathoni <me@zainf.dev>
2023-06-27 20:23:52 +0800

    feat(week-20): add layout in authors/[authorsSlug] page

--- pages/authors/[authorSlug].js
+++ pages/authors/[authorSlug].js
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import Layout from "../../components/Layout";
 
 export default function Author(props) {
   return (
-    <main>
-      <h1>It's {props.authorSlug}!</h1>
-      <h2>This page is generated statically</h2>
-      <Image
-        src="/images/profile.jpg"
-        alt="Profile Picture"
-        width={144}
-        height={144}
-      />
-      <p>
-        <a href="/contact">Contact me</a>
-      </p>
-    </main>
+    <Layout>
+      <main>
+        <h1>It's {props.authorSlug}!</h1>
+        <h2>This page is generated statically</h2>
+        <Image
+          src="/images/profile.jpg"
+          alt="Profile Picture"
+          width={144}
+          height={144}
+        />
+        <p>
+          <a href="/contact">Contact me</a>
+        </p>
+      </main>
+    </Layout>
   );
 }
 
